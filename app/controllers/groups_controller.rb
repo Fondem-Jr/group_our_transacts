231541ddecbaebc820efa4996a0f773797f5c80e
Fondem Junior <fondemjunior@yahoo.com>
2021-08-12 20:37:21 +0100

    Display the groups in order

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -4,7 +4,7 @@ class GroupsController < ApplicationController
 
   # GET /groups or /groups.json
   def index
-    @groups = Group.all
+    @groups = Group.all.order('name')
   end
 
   # GET /groups/1 or /groups/1.json
