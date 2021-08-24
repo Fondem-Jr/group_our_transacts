703509161b70df3f0397923c8e6ae0110796a8c6
Fondem Junior <fondemjunior@yahoo.com>
2021-08-24 01:57:01 +0100

    Add rspec test for group model

---
+++ spec/models/group_spec.rb
@@ -0,0 +1,35 @@
+require 'rails_helper'
+
+RSpec.describe Group, type: :model do
+  before(:all) do
+    @user = User.create(name: 'User')
+  end
+
+  context 'with one created user' do
+    before(:each) do
+      @group = Group.new(name: 'Group', user_id: @user.id)
+    end
+
+    it 'belongs to a user' do
+      @group.user_id = nil
+      @group.save
+      expect(@group.valid?).to eq false
+    end
+
+    it 'creates a new group' do
+      @group.save
+      expect(@group.valid?).to eq(true)
+    end
+
+    it "can not create a group with an empty name" do
+      @group1 = Group.create(name: '')
+      expect(@group1.valid?).to eq false
+    end
+
+    it 'creates a group if the name is unique' do
+      @group.save
+      @group1 = Group.create(name: @group.name.to_s)
+      expect(@group1.valid?).to eq false
+    end
+  end
+end
